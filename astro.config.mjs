f9beadf46e6d781d8596da1dcf2d05afc6d62772
Rafael Said Hernandez Demeneghi <saidrexxdemher@gmail.com>
2024-04-13 19:51:39 -0600

    Config files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import animations from '@midudev/tailwind-animations'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  plugins: [animations],
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#54787d",
+				secondary: "#6b9997",
+				tertiary: "#8ba6ac",
+				quaternary: "#C9D8E5",
+				background: "#E1F1FE",
+			},
+			fontFamily: {
+				Onest: ["Onest Variable", "sans-serif"],
+			}
+		},
 	},
 	plugins: [],
 }
